17ad481225dc1658eff4b3916307add5837b7ce4
steklyanov <steklyanov@gmail.com>
2019-08-29 16:05:41 +0300

    second lesson

---
+++ wordpress_categories/main.py
@@ -0,0 +1,46 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+
+def get_html(url):
+    r = requests.get(url)
+    return r.text
+
+
+def write_csv(data):
+    with open('plugins.csv', 'a') as f:
+        writer = csv.writer(f)
+        writer.writerow((data['name'],
+                         data['url'],
+                         data['reviews']))
+
+
+def refined(s):
+    r = s.split(' ')[0]
+    return r.replace(',', '')
+
+
+def get_data(html):
+    soup = BeautifulSoup(html, 'lxml')
+    featured = soup.find_all('section')[1]
+    plugins = featured.find_all('article')
+    for plugin in plugins:
+        name = plugin.find('h2').text
+        url = plugin.find('h2').find('a').get('href')
+        r = plugin.find('span', class_='rating-count').find('a').text
+        rating = refined(r)
+        data = {'name': name,
+                'url': url,
+                'reviews': rating}
+        write_csv(data)
+    return len(plugins)
+
+
+def main():
+    url = 'https://wordpress.org/plugins/'
+    get_data(get_html(url))
+
+
+if __name__ == '__main__':
+    main()
